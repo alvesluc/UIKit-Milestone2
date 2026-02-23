b8788dab37f16b20ae8d2292801ea7314e4361c7
Lucas Alves <l.alves001@gmail.com>
2026-02-22 23:06:33 -0300

    feat: make UI feel more modern

--- Milestone2/ViewController.swift
+++ Milestone2/ViewController.swift
@@ -1,13 +1,9 @@
-//
-//  ViewController.swift
-//  Milestone2
-//
-//  Created by Macedo on 20/02/26.
-//
-
 import UIKit
 
-class ViewController: UITableViewController {
+class ViewController: UIViewController, UITableViewDelegate {
+    enum Section { case main }
+    private var dataSource: UITableViewDiffableDataSource<Section, String>!
+    
     private var shoppingList: [String] = [
         "Bittersweet Chocolate",
         "White Chocolate",
@@ -16,118 +12,96 @@ class ViewController: UITableViewController {
         "Strawberries"
     ]
     
+    private var tableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-     
-        view.backgroundColor = .systemBackground
+        
         title = "Shopping List"
         navigationController?.navigationBar.prefersLargeTitles = true
         
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Item")
-        
+        setupTableView()
         configureNavigationBar()
+        configureDataSource()
+        updateUI(animated: false)
     }
     
+    private func setupTableView() {
+        tableView = UITableView(frame: view.bounds, style: .insetGrouped)
+        tableView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Item")
+        tableView.delegate = self
+        
+        view.addSubview(tableView)
+    }
+    
+    private func configureDataSource() {
+        dataSource = UITableViewDiffableDataSource<Section, String>(
+            tableView: tableView
+        ) { tableView, indexPath, itemIdentifier in
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Item", for: indexPath)
+            
+            var content = cell.defaultContentConfiguration()
+            content.text = itemIdentifier
+            content.textProperties.font = .preferredFont(forTextStyle: .body)
+            
+            cell.contentConfiguration = content
+            
+            return cell
+        }
+    }
+
+    private func updateUI(animated: Bool = true) {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, String>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(shoppingList, toSection: .main)
+        
+        dataSource.apply(snapshot, animatingDifferences: animated)
+    }
+
     func configureNavigationBar() {
         let clearAction = UIAction { [weak self] _ in
             self?.presentClearListAlert()
         }
-        
         navigationItem.leftBarButtonItem = UIBarButtonItem(
-            title: "Clear", primaryAction: clearAction
+            title: "Clear",
+            primaryAction: clearAction
         )
         
         let addAction = UIAction { [weak self] _ in
             self?.presentAddItemAlert()
         }
-        
         navigationItem.rightBarButtonItem = UIBarButtonItem(
             systemItem: .add,
             primaryAction: addAction
         )
     }
-    
+
     func presentClearListAlert() {
-        let ac = UIAlertController(
-            title: "Clear list",
-            message: "Are you sure that you want to clear your shopping list?",
-            preferredStyle: .actionSheet
-        )
-        
-        let clearListAction = UIAlertAction(
-            title: "Yes, clear list",
-            style: .destructive
-        ) { [weak self] _ in
+        let ac = UIAlertController(title: "Clear list", message: "Are you sure?", preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "Yes, clear list", style: .destructive) { [weak self] _ in
             self?.shoppingList.removeAll()
-            self?.tableView.reloadData()
-        }
-        
-        ac.addAction(clearListAction)
+            self?.updateUI()
+        })
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         
         present(ac, animated: true)
     }
-    
+
     func presentAddItemAlert() {
-        let ac = UIAlertController(
-            title: "Add item",
-            message: nil,
-            preferredStyle: .alert
-        )
-        
-        ac.addTextField { textField in
-            textField.placeholder = "Enter item name"
-        }
-        
-        let addItemAction = UIAlertAction(
-            title: "Create item",
-            style: .default
-        ) { [weak self, weak ac] _ in
-            guard let text = ac?.textFields?[0].text,
-                  !text.trimmingCharacters(in: .whitespaces).isEmpty else { return }
-            
+        let ac = UIAlertController(title: "Add item", message: nil, preferredStyle: .alert)
+        ac.addTextField { $0.placeholder = "Enter item name" }
+        ac.addAction(UIAlertAction(title: "Create item", style: .default) { [weak self, weak ac] _ in
+            guard let text = ac?.textFields?[0].text, !text.isEmpty else { return }
             self?.addItem(text)
-        }
-        
-        ac.addAction(addItemAction)
+        })
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         
         present(ac, animated: true)
     }
-    
+
     func addItem(_ text: String) {
-        let newIndex = shoppingList.count
         shoppingList.append(text)
-        
-        let indexPath = IndexPath(row: newIndex, section: 0)
-        tableView.insertRows(at: [indexPath], with: .automatic)
-    }
-    
-    override func tableView(
-        _ tableView: UITableView,
-        numberOfRowsInSection section: Int
-    ) -> Int {
-        return shoppingList.count
-    }
-    
-    override func tableView(
-        _ tableView: UITableView,
-        cellForRowAt indexPath: IndexPath
-    ) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Item", for: indexPath)
-        
-        var content = cell.defaultContentConfiguration()
-        content.text = shoppingList[indexPath.row]
-        
-        cell.contentConfiguration = content
-        
-        return cell
-    }
-    
-    override func tableView(
-        _ tableView: UITableView,
-        didSelectRowAt indexPath: IndexPath
-    ) {
-        tableView.deselectRow(at: indexPath, animated: true)
+        updateUI()
     }
 }
